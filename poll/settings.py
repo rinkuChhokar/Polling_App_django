07c91386fe7e56a99a74d22e2a26fa3c92fc41d2
rinkuChhokar <91409108+rinkuChhokar@users.noreply.github.com>
2021-09-29 12:33:51 +0530

    Update settings.py

--- poll/settings.py
+++ poll/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sa50=_2)_x7^q$r#+0(!5f1ix(4r@hmplksp-bw#^9)oppz*gx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['pollgo.herokuapp.com','127.0.0.1']
 
